35660092da7f12d272edd4eb1f8b8c1a636974cd
mahesh <mahesheu@gmail.com>
2017-02-11 13:14:08 +0530

    first commit

--- resources/config.php
+++ resources/config.php
@@ -1,12 +1,5 @@
 <?php
  
-/*
-    The important thing to realize is that the config file should be included in every
-    page of your project, or at least any page you want access to these settings.
-    This allows you to confidently use these settings throughout a project because
-    if something changes such as your database credentials, or a path to a specific resource,
-    you'll only need to update it here.
-*/
  
 $config = array(
     "db" => array(      
